3bcfa90dfefa1d4765aa8996bc11775ddd84802b
Ankur Mahiwal <c_ankur.mahiwal@shipt.com>
2021-12-12 12:19:49 +0530

    Updating list question

---
+++ python/basic-data-types/list/lists.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/python-lists/problem
+"""
+Lists are used to store multiple items in a single variable.
+
+Lists are one of 4 built-in data types in Python used to store collections of data,
+  the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+
+The list is changeable, meaning that we can change, add, and remove items in a list after it has been created.
+
+"""
